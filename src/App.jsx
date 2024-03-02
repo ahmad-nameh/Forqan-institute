151f068ed8824d8301aec9702426f07473872c74
unknown <ahmad3399bb@gmail.com>
2024-03-02 21:15:55 +0300

    added login & register & archives

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Login from "./components/Login&register/Login"
+import MainInfoArchive from "./components/Archives/MainInfoArchive";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container">
-      
+    <div>
+      <Router>
+        <NavBar />
+        <MainInfoArchive/>
+      </Router>
+      {/* <Login/> */}
     </div>
   );
 }
