09927b66843272c7b5028ce559007fabd93a2189
firassat <firas.sy.sa@gmail.com>
2024-03-03 00:15:23 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
-import Login from "./components/Login&register/Login"
+import Login from "./components/Login&register/Login";
 import MainInfoArchive from "./components/Archives/MainInfoArchive";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Router>
         <NavBar />
-        <MainInfoArchive/>
+        <MainInfoArchive />
       </Router>
       {/* <Login/> */}
-    <div className="container">
       <Home />
     </div>
   );
